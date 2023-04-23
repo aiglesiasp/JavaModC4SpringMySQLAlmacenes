621067b1aa4db504ca15e1a284a98cc0df445421
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-23 03:28:01 +0200

    Innit Commit

---
+++ JavaModC4SpringMySQLAlmacenes/src/main/java/com/aiglesiasp/javamodc4/springmysql/almacenes/JavaModC4SpringMySqlAlmacenesApplication.java
@@ -0,0 +1,13 @@
+package com.aiglesiasp.javamodc4.springmysql.almacenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaModC4SpringMySqlAlmacenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaModC4SpringMySqlAlmacenesApplication.class, args);
+	}
+
+}
